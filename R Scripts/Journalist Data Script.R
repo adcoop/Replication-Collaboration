38667f97e4c2d00ff81609230af26c17d168ac42
adcoop <adc2166@columbia.edu>
2015-10-20 15:49:29 -0400

    added some external resources

--- R Scripts/Journalist Data Script.R
+++ R Scripts/Journalist Data Script.R
@@ -1,5 +1,8 @@
 #install.packages('readstata13')
+# Make sure you've opened Rstudio using the replication_work.Rproj so that it refers to this project
 library(readstata13)
 dat <- read.dta13('1. Journalist Data/Input Data/journalist_data_nopii.dta')
 
+# Alicia is adding this code
+# Trying again to see what happens
 
