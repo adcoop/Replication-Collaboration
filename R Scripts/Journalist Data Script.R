77f62b33072cdc198a2c625c17f9b1df019de1ff
ley2106 <leyoung2@gmail.com>
2015-10-20 15:54:20 -0400

    lauren line 11

--- R Scripts/Journalist Data Script.R
+++ R Scripts/Journalist Data Script.R
@@ -7,3 +7,5 @@ dat <- read.dta13('1. Journalist Data/Input Data/journalist_data_nopii.dta')
 # Trying again to see what happens
 
 ## Lauren is adding code too!
+
+## Lauren is changing line 11!!!!!
