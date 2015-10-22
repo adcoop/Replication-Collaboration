a708edf82128dac86784f9d23bda1d84f68b7c86
ztchan <z.chan@columbia.edu>
2015-10-22 13:06:50 -0400

    IPW, FE & Regressions Specs 1-3

--- R Scripts/IPW Specs 1-3.R
+++ R Scripts/IPW Specs 1-3.R
@@ -1,51 +1,62 @@
 rm(list=ls(all=T))
 setwd("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)")
 
-# Load dataset
+# Load dataset - Spec 1
 dat1 <- as.data.frame(read.csv("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)/4. Analysis/Matlab Data/voteshare1.csv"))
 names(dat1)
 
 # IPW - Spec 1
 IPW1 = (lm(voteshare_spec1_2014 ~ treatany + voteshare_spec1_2009, data=dat1, weights=wgt_treatany))
 summary(IPW1)
-ATE.IPW1 <- coefficients(IPW1)[2]
-print(ATE.IPW1)
 
+# FE - Spec 1
+FE1 <- lm(voteshare_spec1_2014 ~ treatany + voteshare_spec1_2009 + num_eligible1 + num_eligible2, dat1)
+summary(FE1)
+
+# Regressions - Spec 1
 reg1a <- lm(voteshare_spec1_2014 ~ treatany, data=dat1, weights = wgt_treatany)
 summary(reg1a)
 
 dat1$lit_pc <- 100 - dat1$illit_pc
-reg1b <- lm(voteshare_spec1_2014 ~ treatany + voteshare_spec1_2009 + as.factor(illit_pc) + as.factor(rural_pc) + as.factor(scst_pc), dat1, weights=wgt_treatany)
+reg1b <- lm(voteshare_spec1_2014 ~ treatany + voteshare_spec1_2009 + lit_pc + rural_pc + scst_pc + poll_date1 + poll_date2 + poll_date3 + poll_date4 + poll_date5 + poll_date6 + state_election, dat1, weights=wgt_treatany)
 summary(reg1b)
-print(coefficients(reg1b)[2])
 
-# FE - Spec 1 
-#library(lfe)
-#FE1 <- felm(voteshare_spec1_2014 ~ treatany + voteshare_spec1_2009 | (illit_pc + scst_pc + rural_pc), data=dat1)
-#summary(FE1)
-
-# Load dataset
+# Load dataset - Spec 2
 dat2 <- as.data.frame(read.csv("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)/4. Analysis/Matlab Data/voteshare2.csv"))
 names(dat2)
 
 # IPW - Spec 2
 IPW2 = (lm(voteshare_spec2_2014 ~ treatany + voteshare_spec2_2009, data=dat2, weights=wgt_treatany))
 summary(IPW2)
-ATE.IPW2 <- coefficients(IPW2)[2]
-print(ATE.IPW2)
 
+# FE - Spec 2
+FE2 <- lm(voteshare_spec2_2014 ~ treatany + voteshare_spec2_2009 + num_eligible1 + num_eligible2, dat2)
+summary(FE2)
+
+# Regressions - Spec 2
 reg2a <- lm(voteshare_spec2_2014 ~ treatany, data=dat2, weights = wgt_treatany)
 summary(reg2a)
 
-# Load dataset
+dat2$lit_pc <- 100 - dat2$illit_pc
+reg2b <- lm(voteshare_spec2_2014 ~ treatany + voteshare_spec2_2009 + lit_pc + rural_pc + scst_pc + poll_date1 + poll_date2 + poll_date3 + poll_date4 + poll_date5 + poll_date6 + state_election, dat2, weights=wgt_treatany)
+summary(reg2b)
+
+# Load dataset - Spec 3
 dat3 <- as.data.frame(read.csv("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)/4. Analysis/Matlab Data/voteshare3.csv"))
 names(dat3)
 
 # IPW - Spec 3
 IPW3 = (lm(voteshare_spec3_2014 ~ treatany + voteshare_spec3_2009, data=dat3, weights=wgt_treatany))
 summary(IPW3)
-ATE.IPW3 <- coefficients(IPW3)[2]
-print(ATE.IPW3)
 
+# FE - Spec 3
+FE3 <- lm(voteshare_spec3_2014 ~ treatany + voteshare_spec3_2009 + num_eligible1 + num_eligible2, dat3)
+summary(FE3)
+
+# Regressions - Spec 3
 reg3a <- lm(voteshare_spec3_2014 ~ treatany, data=dat3, weights = wgt_treatany)
 summary(reg3a)
+
+dat3$lit_pc <- 100 - dat3$illit_pc
+reg3b <- lm(voteshare_spec3_2014 ~ treatany + voteshare_spec3_2009 + lit_pc + rural_pc + scst_pc + poll_date1 + poll_date2 + poll_date3 + poll_date4 + poll_date5 + poll_date6 + state_election, dat3, weights=wgt_treatany)
+summary(reg3b)
