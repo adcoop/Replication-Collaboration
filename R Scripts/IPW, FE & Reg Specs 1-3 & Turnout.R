dc9d58fad906adbce62aed72aeef817c2967650d
ztchan <z.chan@columbia.edu>
2015-10-24 09:37:03 -0400

    Regressions of other variables added

--- R Scripts/IPW, FE & Reg Specs 1-3 & Turnout.R
+++ R Scripts/IPW, FE & Reg Specs 1-3 & Turnout.R
@@ -1,6 +1,26 @@
 rm(list=ls(all=T))
 setwd("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)")
 
+# Define randomization test function
+randtest <- function(x,y,fun=mean,reps=10000) {
+  n <- length(x)
+  m <- length(y)
+  data <- c(x,y)
+  results <- numeric(reps)
+  for (i in 1:reps) {
+    simtemp <- sample(data)
+    results[i] <- fun(simtemp[1:n])-fun(simtemp[(n+1):(n+m)])
+  }
+  greater.p <- sum(results >= (fun(x)-fun(y)))/reps
+  less.p <- sum(results <= (fun(x)-fun(y)))/reps
+  test.stat <- abs(fun(x)-fun(y))
+  two.sided.p <- sum(abs(results)>=test.stat)/reps
+  p.values <- c(greater.p, less.p, two.sided.p)
+  names(simtemp) <- c("p.greater.than", "p.less.than",
+                      "two.sided.p")
+  return(list(results,p.values,test.stat))
+}
+
 # Load dataset - Voteshare Spec 1 
 dat1 <- as.data.frame(read.csv("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)/4. Analysis/Matlab Data/voteshare1.csv"))
 names(dat1)
@@ -80,3 +100,27 @@ summary(regTOa)
 datTO$lit_pc <- 100 - datTO$illit_pc
 regTOb <- lm(turnoutrate_2014 ~ treatany + turnoutrate_2009 + lit_pc + rural_pc + scst_pc + poll_date1 + poll_date2 + poll_date3 + poll_date4 + poll_date5 + poll_date6 + state_election, datTO, weights=wgt_treatany)
 summary(regTOb)
+
+# Load dataset - Incumbent
+datInc <- as.data.frame(read.csv("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)/4. Analysis/Matlab Data/incumbent_data.csv"))
+names(datInc)
+
+# IPW - Incumbent
+IPWInc = (lm(incvoteshare_2014 ~ treatany + incvoteshare_2009, data=datInc, weights=wgt_treatany))
+summary(IPWInc)
+
+# Load dataset - Independent
+datInd <- as.data.frame(read.csv("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)/4. Analysis/Matlab Data/independent_data.csv"))
+names(datInd)
+
+# IPW - Independent
+IPWInd = (lm(indvoteshare_2014 ~ treatany + indvoteshare_2009, data=datInd, weights=wgt_treatany))
+summary(IPWInd)
+
+# Load dataset - NOTA
+datNOTA <- as.data.frame(read.csv("/Users/zeno/Dropbox/Green and Vasudevan (2015) Data (1)/4. Analysis/Matlab Data/nota_data.csv"))
+names(datNOTA)
+
+# IPW - NOTA
+IPWNOTA = (lm(notavoteshare_2014 ~ treatany, data=datNOTA, weights=wgt_treatany))
+summary(IPWNOTA)
