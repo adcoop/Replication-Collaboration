594678766faf82949ad8c1b081c6b20b719a7d4d
ley2106 <leyoung2@gmail.com>
2015-10-26 08:04:50 -0400

    updated RI

--- R Scripts/ri_fxn.R
+++ R Scripts/ri_fxn.R
@@ -1,7 +1,7 @@
 #####
 ## Randomization inference script
 #####
-
+# 
 # rm(list=ls())
 # 
 # setwd("/Users/Lauren/Dropbox/Green and Vasudevan (2015) replication")
@@ -13,14 +13,17 @@
 # Z <- dat$treatany
 # Y <- dat$voteshare_spec1_2014
 # 
-#   # create covariate matrix
-# cov2 <- as.matrix(dat[,c('voteshare_spec1_2009')])
-# cov <- as.matrix(dat[,c('rural_pc', 'illit_pc', 'scst_pc', 'voteshare_spec1_2009')])
+#   # identify covariates
+# cov <- c('voteshare_spec1_2009')
+# cov <- c('voteshare_spec1_2009', 'rural_pc', 'illit_pc', 'scst_pc')
+# 
+#   # identify prob of treatment
+# prob <- 'prob_treatany'
 # 
 #   # set iterations
 # iter = 10000
 
-ri <- function(dat, Z, Y, cov = NULL, iter = 10000) {
+ri <- function(dat, Z, Y, cov = NULL, prob = NULL, iter = 10000) {
   
   library(readstata13)
   library(foreign)
@@ -34,53 +37,59 @@ ri <- function(dat, Z, Y, cov = NULL, iter = 10000) {
   # set output structure 
   results <- data.frame('ate' = NA, 'lo95' = NA, 'up95' = NA, 'p' = NA)
   
-  # make sure cov is a matrix
-  cov <- as.matrix(cov)
-    
+  # define vars
+  Z <- dat[,Z]
+  Y <- dat[,Y]
+  cov <- as.matrix(dat[,cov])
+  prob <- dat[,prob]
+  obs <- nrow(dat)
+  
   # estimate ate
-  ate <-  estate(Y, Z, X = cov, prob=dat$prob_treatany)
+  ate <- estate(Y, Z, X=cov, prob=prob)
   results$ate <- ate
   
   # generate permutations of treatment by station
   perms <- genperms(block$treat, blockvar = block$block, maxiter=iter)
   perms <- as.data.frame(perms)
   perms <- cbind.data.frame('station_id' = block$station_id, perms)
-  
+    
   # merge station-level perms into table of stations by AC
     # define AC-level id to sort
-  dat$ac_id <- seq(1, nrow(dat))
+  dat$ac_id <- seq(1, obs)
   perms <- as.data.table(perms)
-    # create frame of ACs and stations and merge perms into AC list - station 1
-  perms1 <- as.data.table(subset(dat, select = c('ac_id', 'station_id1')))
-  setnames(perms1, 'station_id1', 'station_id')
-  perms1 <- merge(perms1, perms, all.x = T, by = 'station_id')
-    # create frame of ACs and stations and merge perms into AC list - station 2
-  perms2 <- as.data.table(subset(dat, select = c('ac_id', 'station_id2')))
-  setnames(perms2, 'station_id2', 'station_id')
-  perms2 <- merge(perms2, perms, all.x = T, by = 'station_id')
-    # sort perms for all stations by AC
-  perms1 <- as.matrix(perms1[order(perms1$ac_id),])
-  perms2 <- as.matrix(perms2[order(perms2$ac_id),])
-
+  # merge station treatment into AC for each 1-3 stations
+  for (i in 1:3){
+    # pull out the identifiers from the ac data
+    permsi <- as.data.table(subset(dat, select = c('ac_id', paste0('station_id', i))))
+    setnames(permsi, paste0('station_id',i), 'station_id')
+    # merge permutations of treatment into ac data by station
+    permsi <- merge(permsi, perms, all.x = T, by = 'station_id')
+    permsi <- as.matrix(permsi[order(permsi$ac_id),])
+    permsi[is.na(permsi)] <- 0
+    assign(paste0('perms',i), permsi)
+  }
+  
   # create "treatany" var by station combinations
-  perms_final <- matrix(NA, dim(perms1)[1], iter)
-  perms2[is.na(perms2)] <- 0
+  perms_final <- matrix(NA, obs, iter)
   for (i in 1:iter){
-    t <- ifelse(perms1[,i+2]==1 | perms2[,i+2]==1, 1, 0)
+    t <- ifelse(perms1[,i+2]==1 | perms2[,i+2]==1 | perms3[,i+2]==1, 1, 0)
     perms_final[,i] <- t
   }
   hist(colMeans(perms_final))
   
   # get p-value for observed ATE under sharp null
   Ys <- genouts(Y, Z, ate=0)    # generate potential outcomes under sharp null
-  distout <- gendist(Ys, perms_final, prob = dat$prob_treatany)
+  distout <- gendist(Ys, perms_final, cov, prob = prob)
   results$p <- (sum(distout <= ate))/iter
   save <- dispdist(distout, ate, display.plot = T)
+  
+  # generate confidence intervals
   Ys <- genouts(Y, Z, ate=ate)  # generate potential outcomes UNDER THE ASSUMPTION THAT ATE=ESTIMATED ATE
-  distout <- gendist(Ys, perms_final, prob=dat$prob_treatany)
+  distout <- gendist(Ys, perms_final, cov, prob=prob)
   save <- dispdist(distout, ate, display.plot = F) 
   results$lo95 <- save$quantile[1]
   results$up95 <- save$quantile[2]
+  
   return(results)
   
 }
